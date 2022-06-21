adefba3ec2eda2699219145f7ff7b08c7baad1e1
Lam1109 <1031941624@qq.com>
2022-06-21 17:22:29 +0800

    显示天气信息

--- app/src/main/java/com/example/lamweather/util/Utility.java
+++ app/src/main/java/com/example/lamweather/util/Utility.java
@@ -5,6 +5,8 @@ import android.text.TextUtils;
 import com.example.lamweather.db.City;
 import com.example.lamweather.db.Country;
 import com.example.lamweather.db.Province;
+import com.example.lamweather.gson.Weather;
+import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 
 import org.json.JSONArray;
@@ -70,5 +72,17 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    public static Weather handleWeatherResponse(String response) {
+        try{
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
